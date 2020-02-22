145e08e6698b9038a71cd78afe158c858fd7899b
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-22 22:51:14 +0530

    Moved ticTacToeBoard local variable to a field variable and initialized it in setUp method

--- src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
@@ -1,7 +1,6 @@
 package com.kata.tictactoe;
 
-import org.hamcrest.core.Is;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -10,16 +9,20 @@ import static org.junit.Assert.assertThat;
 public class TicTacToeBoardTest {
 
     private static final int FIVE = 5;
+    private TicTacToeBoard ticTacToeBoard;
+
+    @Before
+    public void setUp() {
+        ticTacToeBoard = new TicTacToeBoard();
+    }
 
     @Test
     public void ticTacToeBoardShouldReturnRowLength5AfterInitialization(){
-        TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
         assertThat(ticTacToeBoard.board.length, is(FIVE));
     }
 
     @Test
     public void ticTacToeBoardShouldReturnColumnLength5AfterInitialization() {
-        TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
         assertThat(ticTacToeBoard.board[0].length, is(FIVE));
     }
 }
