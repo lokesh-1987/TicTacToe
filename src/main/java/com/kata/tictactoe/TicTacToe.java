3b50e4aed47862f8070d02dfe9b5ba69e3fc3b37
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-23 17:00:26 +0530

    Added a test to return Game_is_draw message until the game finishes

--- src/main/java/com/kata/tictactoe/TicTacToe.java
+++ src/main/java/com/kata/tictactoe/TicTacToe.java
@@ -3,8 +3,13 @@ package com.kata.tictactoe;
 class TicTacToe {
 
     private static final String GAME_IS_CONTINUE = "Game is Continue!";
+    private TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
 
     String playTicTacToeGame(int row, int col, char player) {
+        ticTacToeBoard.setValue(row, col, player);
+        if(ticTacToeBoard.isBoardFull()) {
+            return "Game is draw!";
+        }
         return GAME_IS_CONTINUE;
     }
 }
--- src/test/java/com/kata/tictactoe/TicTacToeTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeTest.java
@@ -26,4 +26,17 @@ public class TicTacToeTest {
         assertThat(ticTacToe.playTicTacToeGame(0,0,'X'), is(GAME_IS_CONTINUE));
         assertThat(ticTacToe.playTicTacToeGame(0,2,'0'), is(GAME_IS_CONTINUE));
     }
+
+    @Test
+    public void ticTacToeShouldReturn_GameIsDrawMessage_ifThePlayersMoveAlternativelyUntilGameIsFinished() {
+        assertThat(ticTacToe.playTicTacToeGame(0,0,'X'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(0,2,'0'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(0,4,'X'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(2,0,'0'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(2,2,'X'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(2,4,'0'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(4,2,'X'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(4,0,'0'), is(GAME_IS_CONTINUE));
+        assertThat(ticTacToe.playTicTacToeGame(4,4,'X'), is("Game is draw!"));
+    }
 }
