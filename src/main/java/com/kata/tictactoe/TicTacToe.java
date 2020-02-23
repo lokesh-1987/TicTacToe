08c5f857f0062464cd0f39335322c35ba69d441b
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-23 16:10:57 +0530

    Extracted constant for Game is continue

--- src/main/java/com/kata/tictactoe/TicTacToe.java
+++ src/main/java/com/kata/tictactoe/TicTacToe.java
@@ -1,7 +1,10 @@
 package com.kata.tictactoe;
 
 class TicTacToe {
+
+    private static final String GAME_IS_CONTINUE = "Game is Continue!";
+
     String playTicTacToeGame(int row, int col, char player) {
-        return "Game is Continue!";
+        return GAME_IS_CONTINUE;
     }
 }
