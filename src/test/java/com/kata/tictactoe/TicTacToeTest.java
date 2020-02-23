45ebb920a0acb1aefcee3f9e2d40c39e65c86507
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-23 16:12:20 +0530

    Extracted constant for Game is continue in test class

--- src/test/java/com/kata/tictactoe/TicTacToeTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 
 public class TicTacToeTest {
 
+    private static final String GAME_IS_CONTINUE = "Game is Continue!";
+
     @Test
     public void ticTacToeShouldReturn_ContinueMessage_IfPlayer_X_MovesOn_0_0_Position() {
         TicTacToe ticTacToe = new TicTacToe();
-        Assert.assertThat(ticTacToe.playTicTacToeGame(0,0,'X'), Is.is("Game is Continue!"));
+        Assert.assertThat(ticTacToe.playTicTacToeGame(0,0,'X'), Is.is(GAME_IS_CONTINUE));
     }
 }
