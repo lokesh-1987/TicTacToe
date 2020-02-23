bb42b4a1bbac747b72a7fa0151985144ab10b2ee
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-23 17:37:14 +0530

    Extracted constant for Invalid position

--- src/main/java/com/kata/tictactoe/TicTacToe.java
+++ src/main/java/com/kata/tictactoe/TicTacToe.java
@@ -4,11 +4,12 @@ class TicTacToe {
 
     private static final String GAME_IS_CONTINUE = "Game is Continue!";
     private static final String GAME_IS_DRAW = "Game is draw!";
+    private static final String INVALID_POSITION = "Invalid Position";
     private TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
 
     String playTicTacToeGame(int row, int col, char player) {
         if(ticTacToeBoard.isInvalidPosition(row,col)){
-            return "Invalid Position";
+            return INVALID_POSITION;
         }
         ticTacToeBoard.setValue(row, col, player);
         if(ticTacToeBoard.isBoardFull()) {
