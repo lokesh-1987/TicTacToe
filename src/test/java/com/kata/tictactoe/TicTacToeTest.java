644f622e62ea9c9da7a3419fab35654c9c8489f6
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-23 16:33:44 +0530

    Extracted ticTacToe as a field variable and initialized in setUp methiod

--- src/test/java/com/kata/tictactoe/TicTacToeTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeTest.java
@@ -1,5 +1,6 @@
 package com.kata.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -8,16 +9,20 @@ import static org.junit.Assert.assertThat;
 public class TicTacToeTest {
 
     private static final String GAME_IS_CONTINUE = "Game is Continue!";
+    private TicTacToe ticTacToe;
+
+    @Before
+    public void setUp() {
+        ticTacToe = new TicTacToe();
+    }
 
     @Test
     public void ticTacToeShouldReturn_ContinueMessage_IfPlayer_X_MovesOn_0_0_Position() {
-        TicTacToe ticTacToe = new TicTacToe();
         assertThat(ticTacToe.playTicTacToeGame(0,0,'X'), is(GAME_IS_CONTINUE));
     }
 
     @Test
     public void ticTacToeShouldReturn_ContinueMessage_IfPlayerXGoesFirstOn_0_0_And_Player_O_Makes2ndMoveOn_0_2_Position() {
-        TicTacToe ticTacToe = new TicTacToe();
         assertThat(ticTacToe.playTicTacToeGame(0,0,'X'), is(GAME_IS_CONTINUE));
         assertThat(ticTacToe.playTicTacToeGame(0,2,'0'), is(GAME_IS_CONTINUE));
     }
